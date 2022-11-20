61b921415c95b0f17aaa0ba987648452205de3ac
prabin.shrestha <prabin.shrestha@tradeinsur.com>
2022-11-20 07:27:35 +0545

    loadbook

--- loanbook/models.py
+++ loanbook/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.safestring import mark_safe
+from Book.models import Book
+
+from accounts.models import User
+
 
 # Create your models here.
+class LoadBook(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    book = models.OneToOneField(Book, on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at= models.DateField(default=timezone.now)
+
+
+    # def __str__(self):
+    #     return self.name
+
