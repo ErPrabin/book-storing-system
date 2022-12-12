58d25b83c34bc14933be4e6e5c987f565ab57fa8
prabin.shrestha <prabin.shrestha@tradeinsur.com>
2022-12-12 08:57:01 +0545

    donate purchase to book

--- Book/models.py
+++ Book/models.py
@@ -11,6 +11,7 @@ class Book(models.Model):
     author=models.CharField(max_length=200)
     publisher=models.CharField(max_length=200)
     year=models.CharField(max_length=200)
+    book_type=models.CharField(max_length=200)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     created_at=models.DateTimeField(auto_now_add=True, blank=True)
     updated_at= models.DateField(default=timezone.now)
